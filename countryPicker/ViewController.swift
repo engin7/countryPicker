4078568c5c8fb2c0fb204997a4f1f40713f7af4e
Engin KUK <kuk.engin@gmail.com>
2021-03-22 23:12:45 +0300

    country picker added

--- countryPicker/ViewController.swift
+++ countryPicker/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CountryCodePickerDelegate {
+    
+    func countryCodePickerViewControllerDidPickCountry(_ country: CountryCodePickerViewController.Country) {
+         
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pickerDidPressed(_ sender: Any) {
+        let vc = CountryCodePickerViewController()
+        vc.delegate = self
+        if let nav = containingViewController?.navigationController  {
+            nav.pushViewController(vc, animated: true)
+        } else {
+            let nav = UINavigationController(rootViewController: vc)
+            containingViewController?.present(nav, animated: true)
+        }
+    }
+    
+    /// containingViewController looks at the responder chain to find the view controller nearest to itself
+    var containingViewController: UIViewController? {
+        var responder: UIResponder? = self
+        while !(responder is UIViewController) && responder != nil {
+            responder = responder?.next
+        }
+        return (responder as? UIViewController)
+    }
 }
 
